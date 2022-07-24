a385243d580ae019b5c3e0e72362a18345c040fc
preethi k <kpreethiyadav579@gmail.com>
2022-07-24 14:40:10 +0530

    adding update

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
 
+#This script is to insatll ansible latest version
+#Before ansible used to use python2 version
+#Bust now ansible is using python3 version programming code so need to install python first and then install ansible
 yum install python3-pip -y
 pip3 install pip --upgrade
 pip3 install ansible
